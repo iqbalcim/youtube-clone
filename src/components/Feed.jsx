dee829c5721730ce17165fc62d0cef7efaa9efa3
iqbalcim <chairuliqbalmaulana@gmail.com>
2022-08-30 20:23:16 +0700

    second commit

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -35,7 +35,7 @@ const Feed = () => {
           variant="body2"
           sx={{ mt: 1.5, color: "#fff" }}
         >
-          Copyright © 2022 JSM Media
+          Copyright © 2022 ALEG
         </Typography>
       </Box>
 
